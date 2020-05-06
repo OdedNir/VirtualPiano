06333e56ed61b3f1fbfd756da6b533066135873b
OdedNir <odednir@icloud.com>
2020-05-06 12:58:42 +0300

    update functions.js with touch and mouse click

--- functions.js
+++ functions.js
@@ -8,6 +8,36 @@ function keyPress(keys, audioSrc) {
     });
 }
 
+function mousePress(audioSrc) {
+    let kbdKeys = document.querySelectorAll("kbd");
+    let pressedElement;
+    document.addEventListener("click", (ev) => {
+        pressedElement = ev.target;
+        for (let i = 0; i < kbdKeys.length; i++) {
+            if (pressedElement === kbdKeys[i]) {
+                let sound = new Audio(audioSrc[i]);
+                sound.play();
+                break;
+            }
+        }
+    })
+}
+
+function touchPress(audioSrc) {
+    let kbdKeys = document.querySelectorAll("kbd");
+    let pressedElement;
+    document.addEventListener("touchstart", (ev) => {
+        pressedElement = ev.target;
+        for (let i = 0; i < kbdKeys.length; i++) {
+            if (pressedElement === kbdKeys[i]) {
+                let sound = new Audio(audioSrc[i]);
+                sound.play();
+                break;
+            }
+        }
+    })
+}
+
 let whiteKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
 let audioSrcWhite = ["./A.mp3", "./S.mp3", "./D.mp3", "./F.mp3", "./G.mp3", "./H.mp3", "./J.mp3"];
 keyPress(whiteKeys, audioSrcWhite);
@@ -15,3 +45,4 @@ keyPress(whiteKeys, audioSrcWhite);
 let blackKeys = ['W', 'E', 'T', 'Y', 'U'];
 let audioSrcBlack = ["./W.mp3", "./E.mp3", "./T.mp3", "./Y.mp3", "./U.mp3"];
 keyPress(blackKeys, audioSrcBlack);
+mousePress(audioSrcWhite.concat(audioSrcBlack));
